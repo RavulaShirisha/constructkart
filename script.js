8e7957be20acae13b02d95820fc61d3a8115335b
RavulaShirisha <ravulashirisha27@gmail.com>
2026-03-17 12:12:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-"}
 function addToCart(item, price){
  let cart = JSON.parse(localStorage.getItem("cart")) || [];
- cart.push({item, price});
+ cart.push({item: iterm, price: price});
  localStorage.setItem("cart", JSON.stringify(cart));
  alert(item + " added to cart");
+}
