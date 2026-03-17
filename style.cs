08e6f4463d502f8c588f818ecf6ce26749b13975
RavulaShirisha <ravulashirisha27@gmail.com>
2026-03-17 11:53:34 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,5 @@
+}
+body {font-family: Arial;}
+nav {background: black; color: white; padding: 10px;}
+nav a {color: white; margin: 10px; text-decoration: none;}
+button {margin: 10px; padding: 10px;
